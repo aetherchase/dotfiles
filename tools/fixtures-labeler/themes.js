f08bc57ab5d06c97be28e60e43ecc0b3585d12aa
Andrey Matveev <aetherchasey@gmail.com>
2026-07-06 21:51:11 +0300

    feat(wallpapers): theme-data generator for the labeler

---
+++ tools/fixtures-labeler/themes.js
@@ -0,0 +1,458 @@
+window.FIXTURE_THEMES = {
+  "catppuccin": {
+    "palette": [
+      "#1e1e2e",
+      "#89b4fa",
+      "#f38ba8",
+      "#a6e3a1",
+      "#f9e2af",
+      "#89b4fa",
+      "#f5c2e7",
+      "#94e2d5",
+      "#f38ba8",
+      "#a6e3a1",
+      "#f9e2af",
+      "#89b4fa",
+      "#f5c2e7",
+      "#94e2d5"
+    ],
+    "backgrounds": [
+      "/home/keroqq/.local/share/omarchy/themes/catppuccin/backgrounds/1-totoro.png",
+      "/home/keroqq/.local/share/omarchy/themes/catppuccin/backgrounds/2-waves.png",
+      "/home/keroqq/.local/share/omarchy/themes/catppuccin/backgrounds/3-blue-eye.png",
+      "/home/keroqq/.local/share/omarchy/themes/catppuccin/backgrounds/omarchy.png"
+    ]
+  },
+  "catppuccin-latte": {
+    "palette": [
+      "#eff1f5",
+      "#1e66f5",
+      "#d20f39",
+      "#40a02b",
+      "#df8e1d",
+      "#1e66f5",
+      "#ea76cb",
+      "#179299",
+      "#d20f39",
+      "#40a02b",
+      "#df8e1d",
+      "#1e66f5",
+      "#ea76cb",
+      "#179299"
+    ],
+    "backgrounds": [
+      "/home/keroqq/.local/share/omarchy/themes/catppuccin-latte/backgrounds/1-color-fade.png",
+      "/home/keroqq/.local/share/omarchy/themes/catppuccin-latte/backgrounds/omarchy.png"
+    ]
+  },
+  "ethereal": {
+    "palette": [
+      "#060b1e",
+      "#7d82d9",
+      "#ed5b5a",
+      "#92a593",
+      "#e9bb4f",
+      "#7d82d9",
+      "#c89dc1",
+      "#a3bfd1",
+      "#faaaa9",
+      "#c4cfc4",
+      "#f7dc9c",
+      "#c2c4f0",
+      "#ead7e7",
+      "#dfeaf0"
+    ],
+    "backgrounds": [
+      "/home/keroqq/.local/share/omarchy/themes/ethereal/backgrounds/1-cosmic.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/ethereal/backgrounds/2-meadow.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/ethereal/backgrounds/omarchy.png"
+    ]
+  },
+  "everforest": {
+    "palette": [
+      "#2d353b",
+      "#7fbbb3",
+      "#e67e80",
+      "#a7c080",
+      "#dbbc7f",
+      "#7fbbb3",
+      "#d699b6",
+      "#83c092",
+      "#e67e80",
+      "#a7c080",
+      "#dbbc7f",
+      "#7fbbb3",
+      "#d699b6",
+      "#83c092"
+    ],
+    "backgrounds": [
+      "/home/keroqq/.local/share/omarchy/themes/everforest/backgrounds/1-tree-tops.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/everforest/backgrounds/omarchy.png"
+    ]
+  },
+  "flexoki-light": {
+    "palette": [
+      "#fffcf0",
+      "#205ea6",
+      "#d14d41",
+      "#879a39",
+      "#d0a215",
+      "#205ea6",
+      "#ce5d97",
+      "#3aa99f",
+      "#d14d41",
+      "#879a39",
+      "#d0a215",
+      "#4385be",
+      "#ce5d97",
+      "#3aa99f"
+    ],
+    "backgrounds": [
+      "/home/keroqq/.local/share/omarchy/themes/flexoki-light/backgrounds/1-orb.png",
+      "/home/keroqq/.local/share/omarchy/themes/flexoki-light/backgrounds/2-omarchy.png"
+    ]
+  },
+  "gruvbox": {
+    "palette": [
+      "#282828",
+      "#7daea3",
+      "#ea6962",
+      "#a9b665",
+      "#d8a657",
+      "#7daea3",
+      "#d3869b",
+      "#89b482",
+      "#ea6962",
+      "#a9b665",
+      "#d8a657",
+      "#7daea3",
+      "#d3869b",
+      "#89b482"
+    ],
+    "backgrounds": [
+      "/home/keroqq/.local/share/omarchy/themes/gruvbox/backgrounds/1-the-backwater.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/gruvbox/backgrounds/2-flower-basket.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/gruvbox/backgrounds/3-village-square.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/gruvbox/backgrounds/4-idyllic-procession.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/gruvbox/backgrounds/5-leaves.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/gruvbox/backgrounds/omarchy.png"
+    ]
+  },
+  "hackerman": {
+    "palette": [
+      "#0b0c16",
+      "#82fb9c",
+      "#50f872",
+      "#4fe88f",
+      "#50f7d4",
+      "#829dd4",
+      "#86a7df",
+      "#7cf8f7",
+      "#85ff9d",
+      "#9cf7c2",
+      "#a4ffec",
+      "#c4d2ed",
+      "#cddbf4",
+      "#d1fffe"
+    ],
+    "backgrounds": [
+      "/home/keroqq/.local/share/omarchy/themes/hackerman/backgrounds/1-synth-scape.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/hackerman/backgrounds/2-geometric.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/hackerman/backgrounds/omarchy.png"
+    ]
+  },
+  "kanagawa": {
+    "palette": [
+      "#1f1f28",
+      "#7e9cd8",
+      "#c34043",
+      "#76946a",
+      "#c0a36e",
+      "#7e9cd8",
+      "#957fb8",
+      "#6a9589",
+      "#e82424",
+      "#98bb6c",
+      "#e6c384",
+      "#7fb4ca",
+      "#938aa9",
+      "#7aa89f"
+    ],
+    "backgrounds": [
+      "/home/keroqq/.local/share/omarchy/themes/kanagawa/backgrounds/1-kanagawa.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/kanagawa/backgrounds/omarchy.png"
+    ]
+  },
+  "lumon": {
+    "palette": [
+      "#16242d",
+      "#8bc9eb",
+      "#4d86b0",
+      "#5e95bc",
+      "#6fa4c9",
+      "#6fb8e3",
+      "#8bc9eb",
+      "#b4e4f6",
+      "#73a6cb",
+      "#86b7d8",
+      "#9dcae5",
+      "#f2fcff",
+      "#b1d8ee",
+      "#d1eef8"
+    ],
+    "backgrounds": [
+      "/home/keroqq/.local/share/omarchy/themes/lumon/backgrounds/01-united-in-severance.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/lumon/backgrounds/02-opinions-equally.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/lumon/backgrounds/omarchy.png"
+    ]
+  },
+  "matte-black": {
+    "palette": [
+      "#121212",
+      "#e68e0d",
+      "#d35f5f",
+      "#ffc107",
+      "#b91c1c",
+      "#e68e0d",
+      "#d35f5f",
+      "#bebebe",
+      "#b91c1c",
+      "#ffc107",
+      "#b90a0a",
+      "#f59e0b",
+      "#b91c1c",
+      "#eaeaea"
+    ],
+    "backgrounds": [
+      "/home/keroqq/.local/share/omarchy/themes/matte-black/backgrounds/0-ship-at-sea.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/matte-black/backgrounds/1-dark-waters.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/matte-black/backgrounds/2-dot-hands.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/matte-black/backgrounds/omarchy.png"
+    ]
+  },
+  "miasma": {
+    "palette": [
+      "#222222",
+      "#78824b",
+      "#685742",
+      "#5f875f",
+      "#b36d43",
+      "#78824b",
+      "#bb7744",
+      "#c9a554",
+      "#685742",
+      "#5f875f",
+      "#b36d43",
+      "#78824b",
+      "#bb7744",
+      "#c9a554"
+    ],
+    "backgrounds": [
+      "/home/keroqq/.local/share/omarchy/themes/miasma/backgrounds/01-nature-of-fear.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/miasma/backgrounds/02-crowned.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/miasma/backgrounds/omarchy.png"
+    ]
+  },
+  "nord": {
+    "palette": [
+      "#2e3440",
+      "#81a1c1",
+      "#bf616a",
+      "#a3be8c",
+      "#ebcb8b",
+      "#81a1c1",
+      "#b48ead",
+      "#88c0d0",
+      "#bf616a",
+      "#a3be8c",
+      "#ebcb8b",
+      "#81a1c1",
+      "#b48ead",
+      "#8fbcbb"
+    ],
+    "backgrounds": [
+      "/home/keroqq/.local/share/omarchy/themes/nord/backgrounds/0-black-moon.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/nord/backgrounds/1-city-view.png",
+      "/home/keroqq/.local/share/omarchy/themes/nord/backgrounds/2-night-hawks.png",
+      "/home/keroqq/.local/share/omarchy/themes/nord/backgrounds/omarchy.png"
+    ]
+  },
+  "osaka-jade": {
+    "palette": [
+      "#111c18",
+      "#509475",
+      "#ff5345",
+      "#549e6a",
+      "#459451",
+      "#509475",
+      "#d2689c",
+      "#2dd5b7",
+      "#db9f9c",
+      "#63b07a",
+      "#e5c736",
+      "#acd4cf",
+      "#75bbb3",
+      "#8cd3cb"
+    ],
+    "backgrounds": [
+      "/home/keroqq/.local/share/omarchy/themes/osaka-jade/backgrounds/1-glowing-city.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/osaka-jade/backgrounds/2-shaded-entrance.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/osaka-jade/backgrounds/3-mountain-moon.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/osaka-jade/backgrounds/omarchy.png"
+    ]
+  },
+  "retro-82": {
+    "palette": [
+      "#05182e",
+      "#faa968",
+      "#f85525",
+      "#028391",
+      "#e97b3c",
+      "#faa968",
+      "#3f8f8a",
+      "#8cbfb8",
+      "#f85525",
+      "#028391",
+      "#e97b3c",
+      "#faa968",
+      "#3f8f8a",
+      "#8cbfb8"
+    ],
+    "backgrounds": [
+      "/home/keroqq/.local/share/omarchy/themes/retro-82/backgrounds/1-in-the-groove.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/retro-82/backgrounds/2-dusk-guardian.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/retro-82/backgrounds/3-glassy-lines.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/retro-82/backgrounds/4-gateway.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/retro-82/backgrounds/5-zen-boat.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/retro-82/backgrounds/6-abstract-pyramids.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/retro-82/backgrounds/7-the-journey.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/retro-82/backgrounds/8-glitter-glass.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/retro-82/backgrounds/omarchy.png"
+    ]
+  },
+  "ristretto": {
+    "palette": [
+      "#2c2525",
+      "#f38d70",
+      "#fd6883",
+      "#adda78",
+      "#f9cc6c",
+      "#f38d70",
+      "#a8a9eb",
+      "#85dacc",
+      "#ff8297",
+      "#c8e292",
+      "#fcd675",
+      "#f8a788",
+      "#bebffd",
+      "#9bf1e1"
+    ],
+    "backgrounds": [
+      "/home/keroqq/.local/share/omarchy/themes/ristretto/backgrounds/0-launch.png",
+      "/home/keroqq/.local/share/omarchy/themes/ristretto/backgrounds/1-color-curves.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/ristretto/backgrounds/2-coffee-beans.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/ristretto/backgrounds/3-industrial-moon.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/ristretto/backgrounds/omarchy.png"
+    ]
+  },
+  "rose-pine": {
+    "palette": [
+      "#faf4ed",
+      "#56949f",
+      "#b4637a",
+      "#286983",
+      "#ea9d34",
+      "#56949f",
+      "#907aa9",
+      "#d7827e",
+      "#b4637a",
+      "#286983",
+      "#ea9d34",
+      "#56949f",
+      "#907aa9",
+      "#d7827e"
+    ],
+    "backgrounds": [
+      "/home/keroqq/.local/share/omarchy/themes/rose-pine/backgrounds/1-funky-shapes.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/rose-pine/backgrounds/2-dot-map.png",
+      "/home/keroqq/.local/share/omarchy/themes/rose-pine/backgrounds/3-omarchy-plants.png",
+      "/home/keroqq/.local/share/omarchy/themes/rose-pine/backgrounds/omarchy.png"
+    ]
+  },
+  "tokyo-night": {
+    "palette": [
+      "#1a1b26",
+      "#7aa2f7",
+      "#f7768e",
+      "#9ece6a",
+      "#e0af68",
+      "#7aa2f7",
+      "#ad8ee6",
+      "#449dab",
+      "#ff7a93",
+      "#b9f27c",
+      "#ff9e64",
+      "#7da6ff",
+      "#bb9af7",
+      "#0db9d7"
+    ],
+    "backgrounds": [
+      "/home/keroqq/.local/share/omarchy/themes/tokyo-night/backgrounds/0-swirl-buck.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/tokyo-night/backgrounds/1-sunset-lake.png",
+      "/home/keroqq/.local/share/omarchy/themes/tokyo-night/backgrounds/2-pawel-czerwinski.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/tokyo-night/backgrounds/3-milad-fakurian.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/tokyo-night/backgrounds/4-oma-cityscape.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/tokyo-night/backgrounds/5-oma.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/tokyo-night/backgrounds/omarchy.png"
+    ]
+  },
+  "vantablack": {
+    "palette": [
+      "#000000",
+      "#8d8d8d",
+      "#a4a4a4",
+      "#b6b6b6",
+      "#cecece",
+      "#8d8d8d",
+      "#9b9b9b",
+      "#b0b0b0",
+      "#a4a4a4",
+      "#b6b6b6",
+      "#cecece",
+      "#8d8d8d",
+      "#9b9b9b",
+      "#b0b0b0"
+    ],
+    "backgrounds": [
+      "/home/keroqq/.local/share/omarchy/themes/vantablack/backgrounds/0-dot-hands.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/vantablack/backgrounds/1-twisted-stairs.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/vantablack/backgrounds/2-layers-deep.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/vantablack/backgrounds/3-layers-stacked.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/vantablack/backgrounds/omarchy.png"
+    ]
+  },
+  "white": {
+    "palette": [
+      "#ffffff",
+      "#6e6e6e",
+      "#2a2a2a",
+      "#3a3a3a",
+      "#4a4a4a",
+      "#1a1a1a",
+      "#2e2e2e",
+      "#3e3e3e",
+      "#2a2a2a",
+      "#3a3a3a",
+      "#4a4a4a",
+      "#1a1a1a",
+      "#2e2e2e",
+      "#3e3e3e"
+    ],
+    "backgrounds": [
+      "/home/keroqq/.local/share/omarchy/themes/white/backgrounds/1-white.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/white/backgrounds/2-white.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/white/backgrounds/3-white.jpg",
+      "/home/keroqq/.local/share/omarchy/themes/white/backgrounds/omarchy.png"
+    ]
+  }
+};
